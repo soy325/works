d181be5d4f919e1c985b2460cdce955bf074841d
soy325 <ksylove23@naver.com>
2025-02-12 20:06:08 +0900

    내용 수정

--- 10. 시엔느 (animation-slide)/script/index.js
+++ 10. 시엔느 (animation-slide)/script/index.js
@@ -20,20 +20,16 @@ $('button.prev').click(function(){
     })
 })
 
-// tab 메뉴 이벤트
-$('.tab-menu li').click(function(){ // 탭메뉴 li 클릭시
-    let tab =  $(this).find('img').attr("src"); // 누른 li 안에 있는 img 경로 담기
+// 탭 메뉴 이벤트
+$('.tab-menu li').click(function(){ 
+    let tab = $(this).find('img').attr("src");
     let i = $(this).index();
+    let idx = $(this).closest('.hslide').index();
 
-    let idx = $(this).parents('.hslide').index(); // 누른 li 가 몇번 째 슬라이드($('.hslide'))에 있는지 담기
-
-    $('.vslide').eq(idx).css({ 'background-image' : 'url(.'+tab+')'}) // 누른 li 와 같은 슬라이드에 있는 $('.vslide') 의 배경이미지 변경하기
-    
-    //li 클릭했을 때 글내용 바뀜
-    $('.hslide').eq(idx).find('.tab li').fadeOut();
-    $('.hslide').eq(idx).find('.tab li').eq(i).fadeIn();
-
-    // 메뉴가 어두운 상태에서 클릭했을 때 밝아지기
-    $(this).siblings('.tab-menu li').removeClass('on');
-    $(this).addClass('on')
-})
+    $('.vslide').eq(idx).css({ 'background-image': 'url(' + tab + ')' });
+    let tabList = $('.hslide').eq(idx).find('.tab li');
+    tabList.hide(); 
+    tabList.eq(i).fadeIn();
+    $(this).siblings().removeClass('on'); 
+    $(this).addClass('on');
+});
